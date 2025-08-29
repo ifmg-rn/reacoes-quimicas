d9a6e76902adad3f05e0b22766ef80701cc6026a
John <johnaldo.yohanan@gmail.com>
2025-08-29 20:39:12 -0300

    Move as configurações do prettier para um arquivo próprio e melhora a configuração do lint-staged.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -5,6 +5,10 @@
 const settings = {
 	"src/**/*": [() => "tsc", "eslint --fix", "prettier --write"],
 	"styles/**/*": ["stylelint --fix", "prettier --write"],
+	"tsconfig.json|pnpm-lock.yaml": [() => "tsc", "prettier --write"],
+	"eslint.config.mjs": [() => "eslint", () => "prettier --check ."],
+	"stylelint.config.mjs": [() => "stylelint .", () => "prettier --check ."],
+	"prettier.config.mjs": [() => "prettier --check ."],
 };
 settings[`!(${Object.keys(settings).join("|")})`] = [
 	"prettier --write --ignore-unknown",
